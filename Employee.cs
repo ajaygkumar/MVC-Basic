3c3c67e212cffe6d75b4ea3572940ba2065db989
ajaygkumar <31571077+ajaygkumar@users.noreply.github.com>
2017-09-23 23:33:04 -0500

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+   
+        public class Employee
+        {
+            public string firstname { get; set; }
+            public string lastname { get; set; }
+            public string address { get; set; }
+
+
+        }
+    
+}
---
+++ HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Employee e = new Employee() { firstname="ajay", lastname="g", address="texas"};
+
+
+            return View(e);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
